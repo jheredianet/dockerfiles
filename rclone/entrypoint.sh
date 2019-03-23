af976f8ecb0c95fe4cee6b47f859af39900e9886
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2019-03-23 17:27:39 +0100

    Fix rclone cache expire

--- rclone/entrypoint.sh
+++ rclone/entrypoint.sh
@@ -27,6 +27,7 @@ trap disconnect  SIGTERM
 /usr/bin/rclone mount --rc --allow-other \
     --fast-list --log-level INFO \
     --vfs-read-chunk-size-limit off \
+    --poll-interval 0 \
     --buffer-size $BUFFER_SIZE \
     --dir-cache-time $DIR_CACHE_TIME \
     --drive-chunk-size $DRIVE_CHUNK_SIZE \
