57a6e78d9d39504049c01e45f74e9023d4684d03
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2019-04-16 00:01:59 +0200

    --stats-log-level NOTICE and net tools

--- restic/install.sh
+++ restic/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "install rclon tool and dependencies"
-echo "Begining..."
+echo "Installing Rclone tool and dependencies"
 apt-get update -qq
-apt-get install -y nano man curl unzip fuse restic
+apt-get install -y nano man curl unzip fuse htop nload
 curl https://rclone.org/install.sh | bash
+
+echo "Updating restic..."
+restic self-update
 
 echo "cleanup compilations tools"
 apt autoremove -y && apt autoclean -y && apt clean -y
