21760da2ab26383ccabab72c4edc39c3b6a9f376
jheredianet <jchm@infoinnova.net>
2019-04-28 19:24:49 +0200

    Install tzdata

--- restic/install.sh
+++ restic/install.sh
@@ -2,7 +2,7 @@
 
 echo "Updating Linux..."
 apt-get update -qq
-apt-get install -y nano man curl unzip fuse htop nload mc
+apt-get install -y nano man curl unzip fuse htop nload mc tzdata
 echo "Installing 'rclone' tool and dependencies..."
 curl https://rclone.org/install.sh | bash
 
