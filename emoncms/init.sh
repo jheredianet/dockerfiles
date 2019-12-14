a059fcd8336ffd875825c5a35e1601203a278699
jheredianet <jchm@infoinnova.net>
2019-12-14 03:36:51 +0100

    fix updates

--- emoncms/init.sh
+++ emoncms/init.sh
@@ -5,10 +5,10 @@ mkdir -p /var/log/emoncms/
 echo "" >> /var/log/emoncms/emoncms.log 
 echo "" >> /var/log/emoncms/emonpiupdate.log
 
-#update
+# update from git
 
-git clone https://github.com/emoncms/emoncms.git /var/www/html
-git clone https://github.com/emoncms/dashboard.git /var/www/html/Modules/dashboard
-git clone https://github.com/emoncms/graph.git /var/www/html/Modules/graph
-git clone https://github.com/emoncms/app.git /var/www/html/Modules/app
-git clone https://github.com/emoncms/device.git /var/www/html/Modules/device
+cd /var/www/html && git pull origin master
+cd /var/www/html/Modules/dashboard && git pull origin master
+cd /var/www/html/Modules/graph && git pull origin master
+cd /var/www/html/Modules/app && git pull origin master
+cd /var/www/html/Modules/device && git pull origin master
