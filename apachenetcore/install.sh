c307119a963d03a7e412bd2cd1b98d42160387c1
jheredianet <jchm@infoinnova.net>
2021-05-01 08:47:18 +0200

    update to dotnet-sdk-5.0

--- apachenetcore/install.sh
+++ apachenetcore/install.sh
@@ -8,7 +8,7 @@ apt-get update; apt-get install -y wget apt-transport-https
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 
-apt-get update && apt-get install -y dotnet-sdk-2.1 
+apt-get update && apt-get install -y dotnet-sdk-5.0 
 
 echo "cleanup compilations tools"
 apt autoremove -y && apt autoclean -y && apt clean -y
