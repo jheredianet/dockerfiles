bc3511a560882205819d7a7c2bd5718be5a33e0a
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2022-02-15 09:49:50 +0100

    update teslamate-abrp repository

--- teslamate-abrp/install.sh
+++ teslamate-abrp/install.sh
@@ -2,6 +2,7 @@ echo "install tools and dependencies"
 echo "Begining..."
 apk update -qq
 apk add nano git -qq
-git clone https://github.com/fetzu/teslamate-abrp.git /teslamate-abrp
+#git clone https://github.com/fetzu/teslamate-abrp.git /teslamate-abrp
+git clone https://github.com/letienne/teslamate-abrp.git /teslamate-abrp
 pip install --no-cache-dir -r /teslamate-abrp/requirements.txt
 echo "Done."
