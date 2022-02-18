f1cb420bd62f26c507de3a0ac3f4c36bce14f800
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2022-02-18 11:33:00 +0100

    update Teslamate ABRP to my own repository

--- teslamate-abrp/install.sh
+++ teslamate-abrp/install.sh
@@ -3,6 +3,7 @@ echo "Begining..."
 apk update -qq
 apk add nano git -qq
 #git clone https://github.com/fetzu/teslamate-abrp.git /teslamate-abrp
-git clone https://github.com/letienne/teslamate-abrp.git /teslamate-abrp
+#git clone https://github.com/letienne/teslamate-abrp.git /teslamate-abrp
+git clone https://github.com/jheredianet/teslamate-abrp.git /teslamate-abrp
 pip install --no-cache-dir -r /teslamate-abrp/requirements.txt
 echo "Done."
