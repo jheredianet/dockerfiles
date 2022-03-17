f239ca99a8f7c8cce1e134360143f772c361deb4
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2022-03-17 09:06:09 +0100

    teslamate change branch

--- teslamate-abrp/install.sh
+++ teslamate-abrp/install.sh
@@ -4,6 +4,6 @@ apk update -qq
 apk add nano git -qq
 #git clone https://github.com/fetzu/teslamate-abrp.git /teslamate-abrp
 #git clone https://github.com/letienne/teslamate-abrp.git /teslamate-abrp
-git clone https://github.com/jheredianet/teslamate-abrp.git /teslamate-abrp
+git clone --branch feature_moderatedtime https://github.com/jheredianet/teslamate-abrp.git /teslamate-abrp
 pip install --no-cache-dir -r /teslamate-abrp/requirements.txt
 echo "Done."
