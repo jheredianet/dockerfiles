f956bf31358ae25a904238dee158e806a4e3407b
Juan Carlos Heredia Mayer <jchm@infoinnova.net>
2022-08-30 11:12:29 +0200

    update requeriments for PostgreSQL

--- teslamate-abrp/install.sh
+++ teslamate-abrp/install.sh
@@ -5,5 +5,7 @@ apk add nano git -qq
 #git clone https://github.com/fetzu/teslamate-abrp.git /teslamate-abrp
 #git clone https://github.com/letienne/teslamate-abrp.git /teslamate-abrp
 git clone --branch feature_moderatedtime https://github.com/jheredianet/teslamate-abrp.git /teslamate-abrp
+python -m pip install --upgrade pip
 pip install --no-cache-dir -r /teslamate-abrp/requirements.txt
+pip install psycopg2-binary
 echo "Done."
