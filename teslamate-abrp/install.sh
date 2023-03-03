c1ea5f581c440ecd75a00206642e98a340f8da15
Juan Carlos Heredia <jchm@infoinnova.net>
2023-03-03 17:03:30 +0100

    fix req tm-abrp

--- teslamate-abrp/install.sh
+++ teslamate-abrp/install.sh
@@ -3,6 +3,5 @@ echo "Begining..."
 apk update -qq
 apk add nano git -qq
 python -m pip install --upgrade pip
-pip install --no-cache-dir -r /teslamate-abrp/requirements.txt
 pip install psycopg2-binary
 echo "Done."
--- teslamate-abrp/run.sh
+++ teslamate-abrp/run.sh
@@ -5,6 +5,7 @@ if [ -d "/teslamate-abrp" ]; then
 else # if not pull for the first time
     echo "Getting repository for the firstime..."
     git clone --branch $GIT_BRANCH https://github.com/jheredianet/teslamate-abrp.git /teslamate-abrp
+    pip install --quiet --no-color --no-cache-dir -r /teslamate-abrp/requirements.txt
 fi
 echo "Running daemon..."
 python -u /teslamate-abrp/teslamate_mqtt2abrp.py -a
