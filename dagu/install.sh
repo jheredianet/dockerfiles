8c14c8a7767d601134539791e4875a8ba12cbeb1
Juan Carlos Heredia <jchm@infoinnova.net>
2023-09-10 00:41:31 +0200

    Updates

--- dagu/install.sh
+++ dagu/install.sh
@@ -2,7 +2,7 @@
 
 #User and permissions setup
 apk update
-apk add --no-cache sudo tzdata curl nano git bash
+apk add --no-cache sudo tzdata curl nano git bash openssh rsync
 addgroup -g ${USER_GID} ${USER}
 adduser ${USER} -h /home/${USER} -u ${USER_UID} -G ${USER} -D -s /bin/bash
 echo ${USER} ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${USER}
