d9672f206a4476913be9560b2df15aa136e0559d
Juan Carlos Heredia <jchm@infoinnova.net>
2023-09-09 22:39:15 +0200

    Add bash to alpine

--- dagu/install.sh
+++ dagu/install.sh
@@ -2,8 +2,8 @@
 
 #User and permissions setup
 apk update
-apk add --no-cache sudo tzdata curl nano git
+apk add --no-cache sudo tzdata curl nano git bash
 addgroup -g ${USER_GID} ${USER}
-adduser ${USER} -h /home/${USER} -u ${USER_UID} -G ${USER} -D -s /bin/ash
+adduser ${USER} -h /home/${USER} -u ${USER_UID} -G ${USER} -D -s /bin/bash
 echo ${USER} ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${USER}
 chmod 0440 /etc/sudoers.d/${USER}
