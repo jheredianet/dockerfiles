e22add877c65411fcbae61445a27779453c8f947
Juan Carlos Heredia <jchm@infoinnova.net>
2023-09-10 20:00:05 +0200

    Add jq to Alpine

--- dagu/install.sh
+++ dagu/install.sh
@@ -2,7 +2,7 @@
 
 #User and permissions setup
 apk update
-apk add --no-cache sudo tzdata curl nano git bash openssh rsync
+apk add --no-cache sudo tzdata curl nano git bash openssh rsync jq
 addgroup -g ${USER_GID} ${USER}
 adduser ${USER} -h /home/${USER} -u ${USER_UID} -G ${USER} -D -s /bin/bash
 echo ${USER} ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/${USER}
