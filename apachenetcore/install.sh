0d4dde9f2b2b7f0722b809d0072514a042bd04ef
Juan Carlos Heredia <jchm@infoinnova.net>
2024-02-28 12:37:19 +0100

    update dotnet-sdk-8.0

--- apachenetcore/install.sh
+++ apachenetcore/install.sh
@@ -8,7 +8,8 @@ apt-get update; apt-get install -y wget apt-transport-https
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 
-apt-get update && apt-get install -y dotnet-sdk-7.0 
+apt-get update && apt-get install -y dotnet-sdk-8.0 
 
 echo "cleanup compilations tools"
 apt autoremove -y && apt autoclean -y && apt clean -y
+--platform=linux/amd64,linux/arm/v7,linux/arm64 
