750f617df5cf6a06d4dd7b3a5e3256fe335b949c
Juan Carlos Heredia <jchm@infoinnova.net>
2024-11-29 22:54:08 +0100

    Updates

--- rclone_serve_webdav/start.sh
+++ rclone_serve_webdav/start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+
 rclone selfupdate
 
 # Cargar secretos desde el archivo
 rclone serve webdav --addr :$WEB_PORT \
     --user $HTTP_USER \
     --pass $HTTP_PASS \
-    --log-file=/tmp/rclone.log \
+    --log-file=$RCLONE_LOG_FILE \
     --log-level $RCLONE_LOG_LEVEL \
     --umask 000 \
     --rc \
@@ -14,4 +16,6 @@ rclone serve webdav --addr :$WEB_PORT \
     --rc-web-gui-no-open-browser \
     --rc-user=$HTTP_USER \
     --rc-pass=$HTTP_PASS \
-    $PATH_TO_SERVE
+    $PATH_TO_SERVE &
+
+tail -f $RCLONE_LOG_FILE
