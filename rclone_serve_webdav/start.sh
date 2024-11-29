97f1c4470dfe865942c323c828e2afdca85edc0d
Juan Carlos Heredia <jchm@infoinnova.net>
2024-11-29 22:12:33 +0100

    RCLONE serve webdav

---
+++ rclone_serve_webdav/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rclone selfupdate
+# Cargar secretos desde el archivo
+rclone serve s3 --addr :$WEB_PORT \
+--user $HTTP_USER \
+--pass $HTTP_PASS \
+--log-file=/tmp/rclone.log \
+--log-level $RCLONE_LOG_LEVEL \
+--umask 000 \
+--rc \
+--rc-web-gui \
+--rc-addr :$RC_PORT \
+--rc-web-gui-no-open-browser \
+--rc-user=$HTTP_USER \
+--rc-pass=$HTTP_PASS \
+$PATH_TO_SERVE
